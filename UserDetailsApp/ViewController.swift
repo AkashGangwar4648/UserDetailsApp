d7c101d950a6ba02ff1873c3d28cf7c98ec9fb9f
Apple <apple@Apples-MacBook-Air.local>
2025-03-16 11:28:37 +0530

    Initial commit

---
+++ UserDetailsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDetailsApp
+//
+//  Created by Apple on 16/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
